7a0c24b0df853c912b7093c08e545fafe5130b0e
datquocngo <shaco14091999@gmail.com>
2019-02-26 08:54:59 +0700

    add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,32 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {CustomerComponent} from './customer/customer.component';
+import {CustomerDetailComponent} from './customer-detail/customer-detail.component';
+import {CustomerCreateComponent} from './customer-create/customer-create.component';
+import {CustomerEditComponent} from './customer-edit/customer-edit.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'customers',
+    component: CustomerComponent
+  },
+  {
+    path: 'customer/:id',
+    component: CustomerDetailComponent
+  },
+  {
+    path: 'customers/create',
+    component: CustomerCreateComponent
+  },
+  {
+    path: 'customer/edit/:id',
+    component: CustomerEditComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CustomerComponent } from './customer/customer.component';
+import { CustomerEditComponent } from './customer-edit/customer-edit.component';
+import { CustomerCreateComponent } from './customer-create/customer-create.component';
+import { CustomerDetailComponent } from './customer-detail/customer-detail.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomerComponent,
+    CustomerEditComponent,
+    CustomerCreateComponent,
+    CustomerDetailComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
